01d9718139e708209b0d0beae429cd3e8293f63f
Daniel Riemslag <daniel.riemslag@klirr.se>
2013-09-04 05:01:37 -0700

    default en_US locale

--- src/config/config.php
+++ src/config/config.php
@@ -5,5 +5,5 @@ return array(
 	'secret' => 'APP_SECRET',
 	'namespace' => 'APP_NAMESPACE',
 	'pageId' => 'PAGE_ID',
-	'locale' => 'sv_SE' // en_US, sv_SE, etc https://developers.facebook.com/docs/internationalization/#locales
+	'locale' => 'en_US' // en_US, sv_SE, etc https://developers.facebook.com/docs/internationalization/#locales
 );
